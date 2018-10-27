08ece506b7b66e7784c0e13c1dfbe5590981149d
shirleypaz <shirley.pazz@gmail.com>
2018-10-27 14:24:17 -0300

    Sorteador com aido (falta pause)

--- js/index.js
+++ js/index.js
@@ -1,44 +1,36 @@
 const numberOfStudents = 31;
 
 let btnSort = document.getElementById('btn-sort');
+var audio = new Audio("peao.mp3")
 
 btnSort.addEventListener('click', function () {
     let sorted = sort();
     buildSorted(sorted);
-    playAudio()
+    audio.play();
 });
 
-function playAudio(){
-    var audio = document.getElementById('audio');
-        if (audio.paused) {
-            audio.play();
-        }else{
-            audio.pause();
-            audio.currentTime = 0;
-        }
-}
 
 function sort() {
     return sortRandomNumber(1, numberOfStudents);
 }
 
 function sortRandomNumber(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
-
-// function buildSorted(sorted) {
-//     let divSorted = document.getElementById('sorted');
-//     divSorted.innerHTML = "<img alt='Sorted' src='students/" + sorted + ".jpg' />";
-// }
 
 function buildSorted(sorted) {
     let divSorted = document.getElementById('sorted');
     for (let i = 1; i <= numberOfStudents; i++) {
-        if (i === sorted) { break; }
-        setTimeout(function() {
-            divSorted.innerHTML = "<img class='circle' alt='Sorted' src='students/" + i + ".jpg' />"}
-            , i*30)
-    }   
+        if (i === sorted) {
+            console.log(audio.paused, 'audio')
+            audio.pause();
+            break;
+        }
+        setTimeout(function () {
+            divSorted.innerHTML = "<img class='circle' alt='Sorted' src='students/" + i + ".jpg' />"
+        }
+            , i * 100)
+    }
 }
 
 
